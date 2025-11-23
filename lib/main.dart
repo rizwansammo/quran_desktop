8a48cf22d8e9ad797f5c41b5d0856903569ba1ab
Rizwan Sammo <mrizwan.sammo@gmail.com>
2025-11-23 21:46:01 +0600

    main.dart Modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'src/app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Temporary fix to clear all saved settings
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.clear(); // CLEAR EVERYTHING ONCE
 
   // Initialize SQLite for Desktop
   sqfliteFfiInit();
